b9019b8ad7b27aed82dbb1d3f233f411f1698b63
DangKhoa1007 <khoazred@gmail.com>
2026-06-27 18:23:22 +0700

    Update new

--- mainInputOutput.c
+++ mainInputOutput.c
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include "function_name.h"
-
 int main() {
 	int choice, subChoice;
-    
-    // Vòng lặp Menu cho phép người dùng chọn chức năng
+
+    // Vong lap Menu cho phep nguoi dung chon chuc nang
     while (1) {
         printf("\n========== MENU QUAN LY LO HANG ==========\n");
         printf("1. Them thong tin lo hang (Add IO)          \n");
         printf("2. In thong tin cac lo hang (Print IO)      \n");
         printf("3. How you want to remove (Remove IO)       \n");
         printf("4. Search batch by ID                       \n");
-        printf("5. Thoat chuong trinh                       \n");
+        printf("5. Gom nhom theo Destination (Group By)      \n");
+        printf("6. Theo doi giao hang (Track Delivery)       \n");
+        printf("7. Sap xep theo Batch ID (Sort)               \n");
+        printf("8. Thoat chuong trinh                       \n");
         printf("Nhap lua chon cua ban: ");
         scanf("%d", &choice);
-        while (getchar() != '\n'); // Xóa bộ đệm sau khi nhập số
-        
+        while (getchar() != '\n'); // Xoa bo dem sau khi nhap so
+
         switch (choice) {
             case 1:
                 addIO();
@@ -37,7 +39,16 @@ int main() {
 			case 4:
 				searchByID();
 				break;
-            case 5:
+			case 5:
+				groupByDestination();
+				break;
+			case 6:
+				trackDelivery();
+				break;
+			case 7:
+				sortByID();
+				break;
+            case 8:
                 printf("Da thoat chuong trinh.\n");
                 exit(0);
             default:
